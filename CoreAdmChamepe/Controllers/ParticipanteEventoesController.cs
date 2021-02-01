06987b1e5931c343ec897e66b7677e200227247b
Lucas Sampaio <sampaiolucas2012@gmail.com>
2021-02-01 01:59:40 -0300

    Primeiro COmmit

---
+++ CoreAdmChamepe/Controllers/ParticipanteEventoesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CoreAdmChamepe.Models;
+using CoreAdmChamepe.Models.Context;
+
+namespace CoreAdmChamepe.Controllers
+{
+    public class ParticipanteEventoesController : Controller
+    {
+        private CoreAdmChamepeDbEntities db = new CoreAdmChamepeDbEntities();
+        private ApplicationDbContext dbUser = new ApplicationDbContext();
+
+        // GET: ParticipanteEventoes
+        public ActionResult Index()
+        {
+            IEnumerable<ParticipanteEvento> participanteEventoes = new List<ParticipanteEvento>();
+            if (User.IsInRole("Administrador"))
+            {
+                 participanteEventoes = db.ParticipanteEventoes.Include(p => p.Evento).Include(p => p.Igreja);
+            }
+            else
+            {
+                 participanteEventoes = db.ParticipanteEventoes.Where(p => p.UserId == dbUser.Users.Where(x => x.UserName.Equals(User.Identity.Name)).FirstOrDefault().Id).Include(p => p.Evento).Include(p => p.Igreja);
+            }
+
+            ViewBag.TotalGeralPessoasCadastradas = db.ParticipanteEventoes.Count();
+            return View(participanteEventoes.ToList());
+        }
+
+        // GET: ParticipanteEventoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParticipanteEvento participanteEvento = db.ParticipanteEventoes.Find(id);
+            if (participanteEvento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participanteEvento);
+        }
+
+        // GET: ParticipanteEventoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.EventoId = new SelectList(db.Eventoes, "EventoId", "EventoDescricao");
+            ViewBag.IgrejaId = new SelectList(db.Igrejas, "IgrejaId", "IgrejaDescricao");
+            return View();
+        }
+
+        // POST: ParticipanteEventoes/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PartcipanteNomeCompleto,ParticipanteRg,ParticipanteEmail,ParticipanteTelefone,ParticipanteIdade,IgrejaId,EventoId")] ParticipanteEvento participanteEvento)
+        {
+            if (ModelState.IsValid)
+            {                
+                if (db.Eventoes.Where(x => x.EventoId == participanteEvento.EventoId).FirstOrDefault().EventoLimitePessoas <= db.ParticipanteEventoes.Count())
+                {
+                     new NotImplementedException("A quantidade de pessoas limite para o evento foi alcançada! Não é possivel cadastrar mais!");
+                }
+                else
+                {
+                    participanteEvento.UserId = dbUser.Users.Where(x => x.UserName.Equals(User.Identity.Name)).FirstOrDefault().Id;
+                    db.ParticipanteEventoes.Add(participanteEvento);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }               
+            }
+
+            ViewBag.EventoId = new SelectList(db.Eventoes, "EventoId", "EventoDescricao", participanteEvento.EventoId);
+            ViewBag.IgrejaId = new SelectList(db.Igrejas, "IgrejaId", "IgrejaDescricao", participanteEvento.IgrejaId);
+            return View(participanteEvento);
+        }
+
+        // GET: ParticipanteEventoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParticipanteEvento participanteEvento = db.ParticipanteEventoes.Find(id);
+            if (participanteEvento == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EventoId = new SelectList(db.Eventoes, "EventoId", "EventoDescricao", participanteEvento.EventoId);
+            ViewBag.IgrejaId = new SelectList(db.Igrejas, "IgrejaId", "IgrejaDescricao", participanteEvento.IgrejaId);
+            return View(participanteEvento);
+        }
+
+        // POST: ParticipanteEventoes/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ParticipanteEventoId,PartcipanteNomeCompleto,ParticipanteRg,ParticipanteEmail,ParticipanteTelefone,ParticipanteIdade,IgrejaId,EventoId")] ParticipanteEvento participanteEvento)
+        {
+            if (ModelState.IsValid)
+            {
+                participanteEvento.UserId = dbUser.Users.Where(x => x.UserName.Equals(User.Identity.Name)).FirstOrDefault().Id;
+                db.Entry(participanteEvento).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EventoId = new SelectList(db.Eventoes, "EventoId", "EventoDescricao", participanteEvento.EventoId);
+            ViewBag.IgrejaId = new SelectList(db.Igrejas, "IgrejaId", "IgrejaDescricao", participanteEvento.IgrejaId);
+            return View(participanteEvento);
+        }
+
+        // GET: ParticipanteEventoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParticipanteEvento participanteEvento = db.ParticipanteEventoes.Find(id);
+            if (participanteEvento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participanteEvento);
+        }
+
+        // POST: ParticipanteEventoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ParticipanteEvento participanteEvento = db.ParticipanteEventoes.Find(id);
+            db.ParticipanteEventoes.Remove(participanteEvento);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CoreAdmChamepe/Models/Context/ParticipanteEvento.cs
@@ -0,0 +1,31 @@
+﻿namespace CoreAdmChamepe.Models.Context
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public partial class ParticipanteEvento
+    {        
+        public int ParticipanteEventoId { get; set; }
+        [Display(Name ="Nome Completo")]
+        public string PartcipanteNomeCompleto { get; set; }
+        [Display(Name = "Rg")]
+        public string ParticipanteRg { get; set; }
+        [Display(Name = "Email")]
+        public string ParticipanteEmail { get; set; }
+        [Display(Name = "Nº Telefone")]
+        public string ParticipanteTelefone { get; set; }
+
+        [Display(Name = "Idade")]
+        public int ParticipanteIdade { get; set; }
+        [Display(Name = "Igreja")]
+        public int IgrejaId { get; set; }
+        [Display(Name = "Evento")]
+        public int EventoId { get; set; }
+        [Display(Name = "Usuário")]
+        public string UserId { get; set; }
+
+        public virtual Evento Evento { get; set; }
+        public virtual Igreja Igreja { get; set; }
+    }
+}
